b0d9cb3248367b99327e6d504136809458fa12f4
Niranjan Thilakarathna <wt@netit-services.com>
2024-05-26 11:49:54 +0530

    has loop in linkedlist

---
+++ src/datastructures/linkedlist/HasLoop.java
@@ -0,0 +1,103 @@
+package datastructures.linkedlist;
+
+/**
+ * Write a method called hasLoop that is part of the linked list class.
+ * The method should be able to detect if there is a cycle or loop present in the linked list.
+ *
+ * This algorithm uses two pointers: a slow pointer and a fast pointer. The slow pointer moves one
+ * step at a time, while the fast pointer moves two steps at a time. If there is a loop in the
+ * linked list, the two pointers will eventually meet at some point. If there is no loop,
+ * the fast pointer will reach the end of the list.
+ */
+public class HasLoop {
+	private Node head;
+	private Node tail;
+	private int length;
+
+	class Node {
+		int value;
+		Node next;
+
+		Node(int value) {
+			this.value = value;
+		}
+	}
+
+	public HasLoop(int value) {
+		Node newNode = new Node(value);
+		head = newNode;
+		tail = newNode;
+		length = 1;
+	}
+
+	public Node getHead() {
+		return head;
+	}
+
+	public Node getTail() {
+		return tail;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void printList() {
+		Node temp = head;
+		while (temp != null) {
+			System.out.println(temp.value);
+			temp = temp.next;
+		}
+	}
+
+	public void printAll() {
+		if (length == 0) {
+			System.out.println("Head: null");
+			System.out.println("Tail: null");
+		} else {
+			System.out.println("Head: " + head.value);
+			System.out.println("Tail: " + tail.value);
+		}
+		System.out.println("Length:" + length);
+		System.out.println("\nLinked List:");
+		if (length == 0) {
+			System.out.println("empty");
+		} else {
+			printList();
+		}
+	}
+
+	public void makeEmpty() {
+		head = null;
+		tail = null;
+		length = 0;
+	}
+
+	public void append(int value) {
+		Node newNode = new Node(value);
+		if (length == 0) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			tail.next = newNode;
+			tail = newNode;
+		}
+		length++;
+	}
+
+	public boolean hasLoop() {
+		Node slow = head;
+		Node fast = head;
+
+		while(fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+
+			if(slow == fast) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
